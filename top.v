e84a5b85d42f4f237dae714fda1ea6f16e74bd83
Tatev2919 <tatev@karapetian.ru>
2021-01-19 15:33:58 +0400

    111

--- top.v
+++ top.v
@@ -1,7 +1,12 @@
-module top;
-	reg clk,rst;
-	wire [15:0] data_gen;
-	wire [20:0] data_out;
+`include "LFSR.v"
+`include "hamming_code.v"
+
+module top(
+	clk,rst,data_out
+);
+	input              clk,rst;
+	output     [20:0] data_out;
+	wire       [15:0] data_gen;
 
 	LFSR_for_hc lf1(clk,rst,data_gen);
 	hamming_code hc(clk,rst,data_gen,data_out);
